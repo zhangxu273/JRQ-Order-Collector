07e7a115f8c3027a5ae3f2e899040dc6fb010fbf
Zhang Xu <im@program.dog>
2016-11-07 19:55:48 +0800

    修改发信模式为SSL

--- JRQ Order Collector/MailManager.py
+++ JRQ Order Collector/MailManager.py
@@ -5,27 +5,28 @@ import json
 
 
 #发送账号配置
-sender = 'password273@163.com'
-senderAccount = 'password273'
-senderPassword = 'password'
+sender = 'im@program.dog'
+senderAccount = 'im@program.dog'
+senderPassword = 'ioqrsyzkeoyibhah'
 #接收方账号配置
 #receivers = ['im@proram.dog','chenheng_bj@163.com']
 receivers = ['im@program.dog']
 #邮件服务器配置
-server = smtplib.SMTP() 
-server.connect('smtp.163.com',25)  
+server = smtplib.SMTP_SSL() 
+server.connect('smtp.qq.com',465)  
  
 #邮件模板
 MailTitleTpl = '订单{0}的状态发生改变，目前状态{1}'
 MailContentTpl3 = '{0}以{1}的价位，手动平仓{2}的{3}单，盈利{4}'
-
+MailContentTpl0 = '{0}以{1}的价位，手动开仓{2}的{3}单'
 #测试模式 只显示 不发送
-DEBUG_MODE = True
+DEBUG_MODE = False
 
 
 #登陆
 def Login():
 	server.login(senderAccount,senderPassword)
+	print("邮件服务器登陆")
 	return
 
 #登出
@@ -54,6 +55,10 @@ def CreateMailMessage(_name,_json):
 	msg = '';
 	if(int(state) == 3):
 		msg = MailContentTpl3.format(_name,_json['close_price'],_json['symbol'],_json['order_status2'],_json['profit'])
+	elif(int(state) == 0):
+		msg = MailContentTpl0.format(_name,_json['open_price'],_json['symbol'],_json['order_status2'])
+	else:
+		msg = json.dumps(_json)
 	
 	print('msg={0}'.format(msg))
 	return msg
